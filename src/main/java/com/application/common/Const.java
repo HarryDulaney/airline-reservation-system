ca4ad8cad3051a602544ed807dafa3cce3f2ddfb
Harry Dulaney <harry.dulaney@gm.com>
2022-02-05 11:59:33 -0500

    Admin FLight Schedule CRUD operations completed. Migrated all functions to work with dynamic Modal popup.

--- src/main/java/com/application/common/Const.java
+++ src/main/java/com/application/common/Const.java
@@ -4,4 +4,7 @@ public class Const {
     public static final String STATUS_ON_TIME = "On Time";
     public static final String STATUS_CANCELLED = "Cancelled";
     public static final String STATUS_BOOKED = "Booked";
+    public static final String ADMIN_OPERATION_EDIT = "edit";
+    public static final String ADMIN_OPERATION_ADD = "add";
+    public static final String ADMIN_OPERATION_DELETE = "delete";
 }
